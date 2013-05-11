4eeed79b2ae7ba3d7b19f2e098adbabb8d1c412b
Daniel Eliasson <daniel@stilero.com>
2013-05-11 16:34:15 +0200

    Refacored and Removed reference by variable

--- autofbook/elements/register.php
+++ autofbook/elements/register.php
@@ -2,7 +2,7 @@
 /**
 * Description of AutoFBook
 *
-* @version  1.1
+* @version  1.2
 * @author Daniel Eliasson - joomla at stilero.com
 * @copyright  (C) 2012-maj-20 Stilero Webdesign http://www.stilero.com
 * @category Custom Form field
@@ -11,25 +11,36 @@
  
 // no direct access
 defined('_JEXEC') or die('Restricted access');
-class Register{
-    static function getHTML($id){
-        $htmlCode = '<span class="readonly"><a href=\'https://developers.facebook.com/setup\' id=\''.$id.'\' target=\'_blank\'>Create A Facebook App</a></span>';
+
+/**
+ * Class for customising the register form field
+ */
+class JFormFieldRegister extends JFormField {
+    
+    protected $type = 'register';
+    const REGISTER_URL = 'https://developers.facebook.com/setup';
+    /**
+     * Returns the HTML for the form input
+     * @return string HTML
+     */
+    protected function getInput(){
+        $htmlCode = '<span class="readonly">
+                <a href=\''.JFormFieldRegister::REGISTER_URL.'\' id=\''.$this->id.'\' target=\'_blank\'>
+                    Create A Facebook App
+                </a>
+            </span>';
+
         return $htmlCode;
     }
-}
-
-
-    class JFormFieldRegister extends JFormField {
-        protected $type = 'register';
-
-        protected function getInput(){
-            return Register::getHTML($this->id);
-        }
-        
-        protected function getLabel(){
-            $toolTip = JText::_($this->element['description']);
-            $text = JText::_($this->element['label']);
-            $labelHTML = '<label id="'.$this->id.'-lbl" for="'.$this->id.'" class="hasTip" title="'.$text.'::'.$toolTip.'">'.$text.'</label>';
-            return $labelHTML;
-        }
-    }//End Class
+    
+    /**
+     * Returns the Label HTML
+     * @return string HTML
+     */
+    protected function getLabel(){
+        $toolTip = JText::_($this->element['description']);
+        $text = JText::_($this->element['label']);
+        $labelHTML = '<label id="'.$this->id.'-lbl" for="'.$this->id.'" class="hasTip" title="'.$text.'::'.$toolTip.'">'.$text.'</label>';
+        return $labelHTML;
+    }
+}//End Class
