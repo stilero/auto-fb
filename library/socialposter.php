69e931de80ac3cb22bb056ada5d80662cf95b555
Daniel Eliasson <daniel@stilero.com>
2013-12-22 17:39:51 +0100

    Added new class for wrapping up the social posting

---
+++ library/socialposter.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * plg_autofbook
+ *
+ * @version  1.0
+ * @package Stilero
+ * @subpackage plg_autofbook
+ * @author Daniel Eliasson <daniel at stilero.com>
+ * @copyright  (C) 2013-dec-22 Stilero Webdesign (http://www.stilero.com)
+ * @license	GNU General Public License version 2 or later.
+ * @link http://www.stilero.com
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access'); 
+
+class StileroAFBSocialposter{
+    
+    
+}
